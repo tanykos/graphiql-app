1234d3c15a3b93c01989ce27d56cc1c64ec80802
pambaka <pambaka@ya.ru>
2024-08-21 18:32:18 +0200

    feat: add nextjs app router with basic layout

---
+++ src/app/test-route/page.tsx
@@ -0,0 +1,7 @@
+// import { ReactNode } from 'react';
+
+function TestRoute(): React.ReactNode {
+  return <p>Some other page is here</p>;
+}
+
+export default TestRoute;
---
+++ src/components/header/header.tsx
@@ -0,0 +1,7 @@
+export default function Header(): React.ReactNode {
+  return (
+    <header>
+      <p>Here will be a header</p>
+    </header>
+  );
+}
