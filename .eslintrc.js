bd9b3a417908f7aa77c5e4a6561eb0348c4315cd
pambaka <pambaka@ya.ru>
2024-08-21 15:42:07 +0200

    chore: create next app with ts and eslint; add prettier, husky, lint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  ignorePatterns: ['.eslintrc.js', 'next.config.mjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  rules: { '@typescript-eslint/no-explicit-any': 'error' },
+};
