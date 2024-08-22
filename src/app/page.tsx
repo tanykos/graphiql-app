233c520cc79bda1d7c8f95d4cd37569a040c7bab
Tetiana Kostromitska <kostromitskayatanya@gmail.com>
2024-08-22 18:13:07 +0200

    chore: add scss (#12)
    Co-authored-by: Tanya Kostromitskaya <tetian.kos@yandex.ru>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import styles from './page.module.css';
+import styles from './page.module.scss';
 
 export default function Welcome(): React.ReactNode {
   return <p className={styles.p}>Welcome page is here</p>;
