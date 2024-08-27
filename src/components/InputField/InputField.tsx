84e991a7551962e752a6762f2556f18159da4dfb
Tanya Kostromitskaya <kostromitskayatanya@gmail.com>
2024-08-27 12:05:19 +0300

    fix: correct inputs type

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -12,12 +12,19 @@ interface InputFieldProps {
 export function InputField({ id, label, type = 'text', fullWidth = true }: InputFieldProps) {
   const [showPassword, setShowPassword] = useState(false);
 
+  const getInputType = () => {
+    if (type === 'password') {
+      return showPassword ? 'text' : 'password';
+    }
+    return type;
+  };
+
   return (
     <FormControl fullWidth={fullWidth} variant="standard">
       <InputLabel htmlFor={id}>{label}</InputLabel>
       <Input
         id={id}
-        type={type === 'password' && !showPassword ? 'password' : 'text'}
+        type={getInputType()}
         autoComplete="off"
         endAdornment={
           type === 'password' && <PasswordAction showPassword={showPassword} setShowPassword={setShowPassword} />
