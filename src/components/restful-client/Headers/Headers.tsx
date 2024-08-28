fb796756fc6349667b8c6908d769cbe40066d4eb
Gunsnfnr <a.j.p.010203@gmail.com>
2024-08-28 22:50:19 +0300

    feat: restful: add headers row on button click

--- src/components/restful-client/Headers/Headers.tsx
+++ src/components/restful-client/Headers/Headers.tsx
@@ -7,19 +7,24 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { DictionaryContext } from '@/providers/dictionary-provider';
 import { FormControl, TextField } from '@mui/material';
 
 export default function Headers() {
+  const [rows, setRows] = useState([1]);
   const dictionary = useContext(DictionaryContext);
   if (!dictionary) return;
+
+  const handleAddHeader = () => {
+    setRows([...rows, rows.length + 1]);
+  };
 
   return (
     <>
       <div className={style.headersTitleWrapper}>
         <p className={style.headersTitle}>{dictionary.headers}</p>
-        <Button type="submit" variant="contained" size="medium" className={style.button}>
+        <Button type="button" variant="contained" size="medium" className={style.button} onClick={handleAddHeader}>
           {dictionary.addHeader}
         </Button>
       </div>
@@ -32,18 +37,21 @@ export default function Headers() {
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow>
-              <TableCell>
-                <FormControl className={style.nestedInput}>
-                  <TextField label={dictionary.key} variant="outlined" size="small" />
-                </FormControl>
-              </TableCell>
-              <TableCell>
-                <FormControl className={style.nestedInput}>
-                  <TextField label={dictionary.value} variant="outlined" size="small" />
-                </FormControl>
-              </TableCell>
-            </TableRow>
+            {rows &&
+              rows.map((row) => (
+                <TableRow key={`row${row}`}>
+                  <TableCell>
+                    <FormControl className={style.nestedInput}>
+                      <TextField label={dictionary.key} variant="outlined" size="small" />
+                    </FormControl>
+                  </TableCell>
+                  <TableCell>
+                    <FormControl className={style.nestedInput}>
+                      <TextField label={dictionary.value} variant="outlined" size="small" />
+                    </FormControl>
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
