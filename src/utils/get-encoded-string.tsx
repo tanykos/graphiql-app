26856f8fc0d576092ee834929945a63e2de51b57
pambaka <pambaka@ya.ru>
2024-08-31 23:16:17 +0200

    fix: fix base64 url encoding

--- src/utils/get-encoded-string.tsx
+++ src/utils/get-encoded-string.tsx
@@ -1,6 +1,8 @@
 export default function getEncodedString(value: string): string | undefined {
   try {
-    return btoa(value);
+    const base64Str = btoa(value);
+    const base64Url = base64Str.replaceAll('/', '_').replace('+', '-');
+    return base64Url;
   } catch {
     return undefined;
   }
