271e474c78d3b0d349e0dfe86c285c568ddaf118
Gunsnfnr <a.j.p.010203@gmail.com>
2024-08-31 15:06:07 +0300

    feat: change headers table appearance

--- src/components/restful-client/Headers/Headers.tsx
+++ src/components/restful-client/Headers/Headers.tsx
@@ -4,7 +4,6 @@ import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { useContext, useState } from 'react';
@@ -34,23 +33,17 @@ export default function Headers(props: Props) {
     <>
       <div className={style.headersTitleWrapper}>
         <p className={style.headersTitle}>{dictionary.headers}</p>
-        <Button type="button" variant="contained" size="medium" className={style.button} onClick={handleAddHeader}>
+        <Button type="button" variant="outlined" size="medium" className={style.button} onClick={handleAddHeader}>
           {dictionary.addHeader}
         </Button>
       </div>
       <TableContainer component={Paper}>
         <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>{dictionary.key}</TableCell>
-              <TableCell>{dictionary.value}</TableCell>
-            </TableRow>
-          </TableHead>
           <TableBody>
             {rows &&
               rows.map((row) => (
                 <TableRow key={`row${row}`}>
-                  <TableCell>
+                  <TableCell className={style.tdStyle}>
                     <FormControl className={style.nestedInput}>
                       <TextField
                         label={dictionary.key}
@@ -60,7 +53,7 @@ export default function Headers(props: Props) {
                       />
                     </FormControl>
                   </TableCell>
-                  <TableCell>
+                  <TableCell className={style.tdStyle}>
                     <FormControl className={style.nestedInput}>
                       <TextField
                         label={dictionary.value}
