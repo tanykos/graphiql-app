faffe1dfc854d1139a8c89af11f9d27a9f039b80
Gunsnfnr <a.j.p.010203@gmail.com>
2024-08-31 22:12:24 +0300

    refactor: include headers into the fetch request

--- src/api/get-restful-data.tsx
+++ src/api/get-restful-data.tsx
@@ -1,12 +1,7 @@
 import { SearchParams } from '@/types';
 import { MethodType } from '@/types/restful';
-import transformSearchParamsToQueries from '@/utils/transform-search-params-to-queries';
-
 export default async function getRestfulData(method: MethodType, url: string, searchParams?: SearchParams) {
-  let queryParams = '';
-
-  if (searchParams) queryParams = transformSearchParamsToQueries(searchParams);
-  const data: unknown = await fetch(`${url}${queryParams}`, { method: method })
+  const data: unknown = await fetch(url, { method: method, headers: searchParams })
     .then((res) => res.json())
     .catch((error: Error) => {
       console.log(error);
--- src/utils/transform-search-params-to-queries.ts
+++
@@ -1,9 +0,0 @@
-import { SearchParams } from '@/types';
-
-export default function transformSearchParamsToQueries(searchParams: SearchParams): string {
-  let query = '';
-  for (const [key, value] of Object.entries(searchParams)) {
-    query = `${query}${query.includes('?') ? '&' : '?'}${key}=${value}`;
-  }
-  return query;
-}
