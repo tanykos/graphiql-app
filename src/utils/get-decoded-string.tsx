3b050d150a691e8a395ab4ed487ccdf13c645f1c
pambaka <101550679+pambaka@users.noreply.github.com>
2024-09-02 06:11:54 +0200

    fix: fix base64 encoding to be url safe (#43)

--- src/utils/get-decoded-string.tsx
+++ src/utils/get-decoded-string.tsx
@@ -1,6 +1,6 @@
-export default function getDecodedStr(urlParam: string): string | undefined {
+export default function getDecodedStr(base64Url: string): string | undefined {
   try {
-    const base64Str = urlParam.replaceAll('%3D', '=');
+    const base64Str = base64Url.replaceAll('_', '/').replaceAll('-', '+');
     const decodedStr = atob(base64Str);
     return decodedStr;
   } catch {
