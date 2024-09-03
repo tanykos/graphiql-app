a36ea793e33e7a5a52208b12a9555179c1126411
pambaka <pambaka@ya.ru>
2024-09-03 12:29:13 +0200

    feat: add history page, display requests history on it

--- src/const.tsx
+++ src/const.tsx
@@ -5,3 +5,7 @@ export const LOCALES: Locale[] = ['en', 'ru'];
 export const DEFAULT_LOCALE = 'en';
 
 export const METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'] as const;
+
+export const REQUESTS_HISTORY = 'requestsHistory';
+
+export const REQUESTS_SEPARATOR = '*';
