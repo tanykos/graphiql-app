b83f6fed0c4ee1985a20bb3f2b25602f1ca62d73
pambaka <101550679+pambaka@users.noreply.github.com>
2024-09-04 18:08:53 +0200

    fix: fix url decoding (#54)

--- src/utils/get-decoded-string.tsx
+++ src/utils/get-decoded-string.tsx
@@ -1,6 +1,6 @@
-export default function getDecodedStr(base64Url: string): string | undefined {
+export default function getDecodedString(base64Url: string): string | undefined {
   try {
-    const base64Str = base64Url.replaceAll('_', '/').replaceAll('-', '+');
+    const base64Str = base64Url.replaceAll('_', '/').replaceAll('-', '+').replaceAll('%3D', '=');
     const decodedStr = atob(base64Str);
     return decodedStr;
   } catch {
