cfdda1c1074538cff318694674108d01a8f4aed5
pambaka <101550679+pambaka@users.noreply.github.com>
2024-09-04 14:54:09 +0200

    feat: add footer, add aria-label to svg images (#50)
    * feat: add footer, add aria-label to svg images
    
    * fix: translate some titles and aria-labels

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,47 @@
-export default function Footer(): React.ReactNode {
+import Link from 'next/link';
+import SvgImage from '../svg-image/svg-image';
+import styles from './footer.module.scss';
+import getDictionary from '@/app/[lang]/dictionaries';
+import { Locale } from '@/types';
+import { use } from 'react';
+
+export default function Footer({ locale }: { locale: Locale }): React.ReactNode {
+  const dictionary = use(getDictionary(locale));
+
+  const developers = [
+    { name: 'Tetiana', github: 'tanykos' },
+    { name: 'Tanya', github: 'pambaka' },
+    { name: 'Artyom', github: 'gunsnfnr' },
+  ];
+
   return (
-    <header>
-      <p>Here will be a footer</p>
-    </header>
+    <footer className={styles.footer}>
+      <div className={styles.developers}>
+        {developers.map((developer) => {
+          return (
+            <Link
+              href={`https://github.com/${developer.github}`}
+              target={'_blank'}
+              key={developer.github}
+              title={developer.name}
+            >
+              <SvgImage
+                url="/footer-icons-sprite.svg#github"
+                className={styles.github}
+                ariaLabel={`${developer.github} github`}
+              />
+            </Link>
+          );
+        })}
+      </div>
+      <p>2024</p>
+      <Link href="https://rs.school/courses/reactjs" target={'_blank'} title="RS School React">
+        <SvgImage
+          url="/footer-icons-sprite.svg#rss"
+          className={styles.github}
+          ariaLabel={dictionary.icons.rsSchoolReact}
+        />
+      </Link>
+    </footer>
   );
 }
