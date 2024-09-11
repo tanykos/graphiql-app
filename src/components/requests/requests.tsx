87388b5a6954da4aacfec997874edca315506246
Gunsnfnr <a.j.p.010203@gmail.com>
2024-09-11 21:13:50 +0300

    feat: RESTful - save requests to the history page

--- src/components/requests/requests.tsx
+++ src/components/requests/requests.tsx
@@ -5,7 +5,6 @@ import { REQUESTS_SEPARATOR } from '@/const';
 import useLocalStorageHistory from '@/hooks/use-local-storage-history';
 import { DictionaryContext } from '@/providers/dictionary-provider';
 import getLocale from '@/utils/get-locale';
-import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useContext } from 'react';
 import RequestLinkText from './RequestLinkText/RequestLinkText';
@@ -34,9 +33,9 @@ export default function Requests(): React.ReactNode {
       {requests &&
         getRequestsUrlParams(requests)
           .map((urlParams: string[], index: number) => (
-            <Link href={`/${locale}/${urlParams.join('/')}`} className={styles.link} key={index}>
+            <a href={`/${locale}/${urlParams.join('/')}`} className={styles.link} key={index}>
               <RequestLinkText urlParams={urlParams} />
-            </Link>
+            </a>
           ))
           .reverse()}
       {!requests && <p>{dictionary.historyPage.emptyMessage}</p>}
