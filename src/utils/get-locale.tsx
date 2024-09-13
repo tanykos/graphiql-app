544acb1d5f4adbe9b5f47065e0b941f8d8736919
Tanya Kostromitskaya <kostromitskayatanya@gmail.com>
2024-09-13 11:03:01 +0200

    fix: check value of locale from url

--- src/utils/get-locale.tsx
+++ src/utils/get-locale.tsx
@@ -1,4 +1,8 @@
-export default function getLocale(pathname: string): string {
-  const locale = pathname.split('/')[1];
-  return locale ? locale : '';
+import { DEFAULT_LOCALE, LOCALES } from '@/const';
+import { Locale } from '@/types';
+
+export default function getLocale(pathname: string): Locale {
+  const localeUrl = pathname.split('/')[1];
+  const locale = LOCALES.includes(localeUrl as Locale) ? (localeUrl as Locale) : DEFAULT_LOCALE;
+  return locale;
 }
