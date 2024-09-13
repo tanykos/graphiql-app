659589347b7b3b68b7356dbe777b269fc17860a9
Tanya Kostromitskaya <kostromitskayatanya@gmail.com>
2024-09-13 21:59:41 +0200

    refactor: clean code

--- src/__tests__/PasswordAction.test.tsx
+++ src/__tests__/PasswordAction.test.tsx
@@ -1,6 +1,6 @@
 import { PasswordAction } from '@/components/PasswordAction/PasswordAction';
 import { render, screen, fireEvent } from '@testing-library/react';
-import { describe, it, expect, vi } from 'vitest';
+import { describe, it, expect, vi, beforeEach, Mock } from 'vitest';
 
 vi.mock('@mui/icons-material/Visibility', () => ({
   default: () => <div data-testid="VisibilityIcon">Visibility Icon</div>,
@@ -11,41 +11,34 @@ vi.mock('@mui/icons-material/VisibilityOff', () => ({
 }));
 
 describe('PasswordAction Component', () => {
-  it('renders the visibility toggle button', () => {
-    const setShowPassword = vi.fn();
+  let setShowPassword: Mock;
+  let toggleButton: HTMLElement;
 
+  beforeEach(() => {
+    setShowPassword = vi.fn();
     render(<PasswordAction showPassword={false} setShowPassword={setShowPassword} />);
+    toggleButton = screen.getByRole('button', { name: 'toggle password visibility' });
+  });
 
-    const button = screen.getAllByTestId('password-toggle-button');
-
-    expect(button).not.toBeNull();
+  it('renders the visibility toggle button', () => {
+    expect(toggleButton).not.toBeNull();
   });
 
   it('shows VisibilityOff icon when showPassword is true', () => {
-    const setShowPassword = vi.fn();
-
     render(<PasswordAction showPassword={true} setShowPassword={setShowPassword} />);
 
     const icon = screen.getAllByTestId('VisibilityOffIcon');
+
     expect(icon).not.toBeNull();
   });
 
   it('shows Visibility icon when showPassword is false', () => {
-    const setShowPassword = vi.fn();
-
-    render(<PasswordAction showPassword={false} setShowPassword={setShowPassword} />);
-
     const icon = screen.getAllByTestId('VisibilityIcon');
+
     expect(icon).not.toBeNull();
   });
 
   it('calls setShowPassword on button click', () => {
-    const setShowPassword = vi.fn();
-
-    render(<PasswordAction showPassword={false} setShowPassword={setShowPassword} />);
-
-    const toggleButton = screen.getByRole('button', { name: 'toggle password visibility' });
-
     fireEvent.click(toggleButton);
 
     expect(setShowPassword).toHaveBeenCalledTimes(1);
