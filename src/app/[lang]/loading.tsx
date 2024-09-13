87ee77b065bca7b4c11277654dc0661a74085a06
Tanya Kostromitskaya <kostromitskayatanya@gmail.com>
2024-09-13 15:03:31 +0200

    fix: correct srinner position

--- src/app/[lang]/loading.tsx
+++ src/app/[lang]/loading.tsx
@@ -2,7 +2,7 @@ import { Box, CircularProgress } from '@mui/material';
 
 export default function Loading() {
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+    <Box sx={{ display: 'flex', flex: '1', justifyContent: 'center', alignItems: 'center' }}>
       <CircularProgress />
     </Box>
   );
