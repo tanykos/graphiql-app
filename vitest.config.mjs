36f0246c83beb00f33a68af922cac50cdb68c98c
Tanya Kostromitskaya <kostromitskayatanya@gmail.com>
2024-09-14 12:00:45 +0200

    chore: update vitest config

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,7 +10,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': '/src'
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
