e98e12f35d12ad965053892067b93f334acab6e0
Artyom P <126002661+Gunsnfnr@users.noreply.github.com>
2024-09-16 00:01:31 +0300

    Feat/add variables to grapiql form (#116)
    * feat: add variables to grapiql form

--- src/utils/transform-headers-to-queries.ts
+++ src/utils/transform-headers-to-queries.ts
@@ -1,12 +1,16 @@
 export default function transformHeadersToQueries(values: { [key: string]: string }): string {
-  let query = '';
-  for (const [key] of Object.entries(values)) {
-    if (key.includes('key')) {
-      const keyNumber = key.split('_')[1];
-      const keyInputValue = values[key].trim();
-      const valueInputValue = values[`value_${keyNumber}`].trim();
-      query += `${query.includes('?') ? '&' : '?'}${keyInputValue}${keyInputValue || valueInputValue ? '=' : ''}${valueInputValue}`;
+  try {
+    let query = '';
+    for (const [key] of Object.entries(values)) {
+      if (key.includes('key')) {
+        const keyNumber = key.split('_')[1];
+        const keyInputValue = values[key].trim();
+        const valueInputValue = values[`value_${keyNumber}`].trim();
+        query += `${query.includes('?') ? '&' : '?'}${keyInputValue}${keyInputValue || valueInputValue ? '=' : ''}${valueInputValue}`;
+      }
     }
+    return query;
+  } catch {
+    return '';
   }
-  return query;
 }
