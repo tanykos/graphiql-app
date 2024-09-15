3fc4f17fd68a247ddf88f7ca2a594a36aba27fdd
pambaka <pambaka@ya.ru>
2024-09-15 12:04:21 +0200

    test: add tests for Response viewer, Graphiql, getDictionary

---
+++ src/__tests__/GraphiQlForm.test.tsx
@@ -0,0 +1,54 @@
+import GraphiQlForm from '@/components/graphiql-form/graphiql-form';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+import dictionary from '@/app/[lang]/dictionaries/en.json';
+
+vi.mock('react', async (importOriginal) => {
+  const actual: object = await importOriginal();
+  return {
+    ...actual,
+    useContext: () => dictionary,
+  };
+});
+
+vi.mock('next/navigation', async (importOriginal) => {
+  const actual: object = await importOriginal();
+  return {
+    ...actual,
+    useSearchParams: () => '',
+    useRouter: () => ({
+      push: vi.fn(),
+    }),
+    usePathname: () => '',
+  };
+});
+
+const key = 'someKey';
+vi.mock('@/hooks/use-local-storage-history', () => {
+  return {
+    default: () => [
+      '',
+      () => {
+        localStorage.setItem(key, '');
+      },
+    ],
+  };
+});
+
+const setItemSpy = vi.spyOn(Storage.prototype, 'setItem');
+
+describe('GraphiQl Form', () => {
+  it('Saves request to LS on form submit', async () => {
+    render(<GraphiQlForm />);
+
+    const endpoint = screen.getByText(dictionary.graphql.endpointUrl);
+    const input = endpoint.parentElement?.nextElementSibling as HTMLInputElement;
+    fireEvent.change(input, { target: { value: 'someEndpoint' } });
+
+    const submitButton = screen.getByText(dictionary.send);
+    fireEvent.click(submitButton);
+    await waitFor(() => {
+      expect(setItemSpy).toHaveBeenCalledWith(key, '');
+    });
+  });
+});
